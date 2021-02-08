2b4e7571169322640cf6ad829500b82b90378631
Tommaso Lamma <lammatommaso@gmail.com>
2021-02-08 20:52:33 +0100

    default

--- interrupt.c
+++ interrupt.c
@@ -1,22 +1,24 @@
 #include<avr/io.h>
 #include<avr/interrupt.h>
 
-#define CLOCK_RATE 15998976
-#define PRESCALER 10 // Dividing by 1024 is in binary a >> 10
-int COUNTER_VALUE = (CLOCK_RATE >> PRESCALER);
+int interrupt_counter;
 
 ISR(TIMER0_COMPA_vect)
 {
-    //interrupt_counter++;
-    //if( interrupt_counter > 100)
-    //{
+    interrupt_counter++;
+    if( interrupt_counter > 10)
+    {
         PORTB ^= 0b00100000;
-    //}
+        interrupt_counter = 0; //really important!!!
+    }
 }
 
 void main()
 {
+    interrupt_counter = 0;
+
     DDRB |= 0b00100000;
+    PORTB = 0b00000000;
     
     TCCR0A = (1 << WGM01); // Set the 8-bit timer to CTC-mode( Clear Timer on Compare)
     // equivalent to TCCR0A = 0b00000010;
@@ -27,7 +29,7 @@ void main()
     // OCR0A and OCR0B are two possible comparisons
     // When the OCIE0A of TIMSK0 if set and the I-bit too , the interruot is enabled when OCR0A == TCNT0
     SREG = 0b10000000; // (it is the I-bit)
-    OCR0A = COUNTER_VALUE; // Is the number I want to compare
+    OCR0A = 0b11111111; // Is the number I want to compare , 255 is the maximum for 8 bits
     TIMSK0 = (1 << OCIE0A);
     // equivalent to TIMSK0 = 0b00000010;
     // When interrupt occurs the OCF0A bit of TIFR0 is set to 1 ( it is the 0b00000010 )
